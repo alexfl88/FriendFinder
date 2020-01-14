51941ca87f62e7f062f970149830a8d4d5dd29e5
alexfl8 <alexanderfranklee@gmail.com>
2020-01-13 17:11:07 -0800

    adds mostly completed assignment just without the README

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: "Brad Pitt",
+        photo: "https://images.app.goo.gl/b9o5PosMaFNiL5J69",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+        name: "Leonardo DiCaprio",
+        photo: "https://images.app.goo.gl/ryq6kvkoZGkw8sSj6",
+        scores: [5, 2, 5, 3, 4, 1, 3, 1, 5, 5]
+    },
+    {
+        name: "Henry Cavill",
+        photo: "https://images.app.goo.gl/XbReq3dUBkR5CC188",
+        scores: [4, 2, 5, 3, 2, 5, 5, 1, 5, 4]
+    },
+    {
+        name: "Daisy Ridley",
+        photo: "https://images.app.goo.gl/A5yk5mCXqzVeDqeJ7",
+        scores: [5, 5, 3, 1, 4, 5, 3, 2, 4, 5]
+    },
+    {
+        name: "Margot Robbie",
+        photo: "https://images.app.goo.gl/AaKsDnWZKTqFsbJ26",
+        scores: [4, 5, 4, 3, 4, 5, 5, 3, 2, 4]
+    },
+    {
+        name: "Charlize Theron",
+        photo: "https://images.app.goo.gl/yx2XMZ1kWrBg3V789",
+        scores: [5, 1, 4, 4, 5, 1, 2 ,5, 4, 4]
+    }
+];
+
+module.exports = friends;
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,10 @@
+var path = require("path");
+
+module.exprots = function(app) {
+    app.get("/survey", function(req,res){
+        res.sendFile(path.join(__dirname + "/app/public/survey.html"));
+    });
+    app.use(function(req,res){
+        res.sendFile(path.join(__dirname + "/app/public/home.html"));
+    });
+}
